8f6cda0040a74316132f6c9c0ccbf3971f3d964e
Mark Sumegi <sumegi.mark@gmail.com>
2019-11-13 17:44:54 +0100

    added spring boot application for the server

---
+++ server/src/main/java/io/eit/brewpal/BrewPalApp.java
@@ -0,0 +1,11 @@
+package io.eit.brewpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewPalApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BrewPalApp.class, args);
+    }
+}
